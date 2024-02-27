f2d4e053b035c90dd657367252cb84acce67c602
Pedro Munhoz <pedro.munhoz@gmail.com>
2024-02-27 19:59:22 -0300

    initial migration

---
+++ Migrations/20240227225849_InitialMigration.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace game_vision_web_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    GameId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plays",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FileId = table.Column<string>(type: "text", nullable: true),
+                    PlayNumber = table.Column<int>(type: "integer", nullable: true),
+                    Offense = table.Column<string>(type: "text", nullable: true),
+                    Defense = table.Column<string>(type: "text", nullable: true),
+                    Down = table.Column<int>(type: "integer", nullable: true),
+                    Distance = table.Column<int>(type: "integer", nullable: true),
+                    Formation = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Yards = table.Column<int>(type: "integer", nullable: true),
+                    Result = table.Column<string>(type: "text", nullable: true),
+                    FirstDown = table.Column<bool>(type: "boolean", nullable: true),
+                    Touchdown = table.Column<bool>(type: "boolean", nullable: true),
+                    Notes = table.Column<string>(type: "text", nullable: true),
+                    GameId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plays", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Plays_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_GameId",
+                table: "Games",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plays_GameId",
+                table: "Plays",
+                column: "GameId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plays");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
