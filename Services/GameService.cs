0df23985906e538e0d993423838a94760ad8dd35
Pedro Munhoz <pedro.munhoz@gmail.com>
2024-06-10 17:26:27 -0300

    wip DTOs and error return

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -1,20 +1,23 @@
+using AutoMapper;
 using game_vision_web_api.Infrastructure.Database;
+using game_vision_web_api.Models.DTOs;
 using game_vision_web_api.Models.Entities;
 using game_vision_web_api.Models.ViewModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace game_vision_web_api.Services;
 
-public class GameService(GameVisionDbContext dbContext)
+public class GameService(GameVisionDbContext dbContext, IMapper mapper)
 {
     private readonly GameVisionDbContext _dbContext = dbContext;
+    private readonly IMapper _mapper = mapper;
 
-    public async Task<(Game? result, string? error)> Create(GameViewModel model, string prefix)
+    public async Task<(GameDTO?, string?)> Create(GameViewModel model, string teamPrefix)
     {
-        var team = await _dbContext.Teams.Where(x => x.Prefix == prefix).FirstOrDefaultAsync();
+        var team = await _dbContext.Teams.Where(x => x.Prefix == teamPrefix).FirstOrDefaultAsync();
 
         if (team is null)
-            return (result: null, error: "Team not found");
+            return (null, "Team not found");
 
         var entity = new Game
         {
@@ -25,20 +28,28 @@ public class GameService(GameVisionDbContext dbContext)
         await _dbContext.Games.AddAsync(entity);
         await _dbContext.SaveChangesAsync();
 
-        return (entity, null);
+        return (_mapper.Map<GameDTO>(entity), null);
     }
 
-    public async Task<List<Game>> GetByTeamPrefix(string teamPrefix)
+    public async Task<(List<GameDTO>?, string?)> GetByTeamPrefix(string teamPrefix)
     {
+        var team = await _dbContext.Teams.Where(x => x.Prefix == teamPrefix).FirstOrDefaultAsync();
+
+        if (team is null)
+            return (null, "Team not found");
+
         var entities = await _dbContext.Games.Include(x => x.Team).Where(x => x.Team.Prefix == teamPrefix).ToListAsync();
 
-        return entities;
+        return (entities.Select(_mapper.Map<GameDTO>).ToList(), null);
     }
 
-    public async Task<Game?> GetById(long id)
+    public async Task<(GameDTO?, string?)> GetById(long id)
     {
         var entity = await _dbContext.Games.Where(x => x.Id == id).FirstOrDefaultAsync();
 
-        return entity;
+        if (entity is null)
+            return (null, "Game not found");
+
+        return (_mapper.Map<GameDTO>(entity), null);
     }
 }
--- Services/TeamService.cs
+++ Services/TeamService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using game_vision_web_api.Infrastructure.Database;
+using game_vision_web_api.Models.DTOs;
 using game_vision_web_api.Models.Entities;
 using game_vision_web_api.Models.ViewModels;
 using Microsoft.EntityFrameworkCore;
@@ -13,7 +14,7 @@ public class TeamService(GameVisionDbContext dbContext)
 {
     private readonly GameVisionDbContext _dbContext = dbContext;
 
-    public async Task<Team> Create(TeamViewModel model)
+    public async Task<(TeamDTO?, string?)> Create(TeamViewModel model)
     {
         var entity = new Team
         {
@@ -24,20 +25,20 @@ public class TeamService(GameVisionDbContext dbContext)
         await _dbContext.Teams.AddAsync(entity);
         await _dbContext.SaveChangesAsync();
 
-        return entity;
+        return (entity, null);
     }
 
-    public async Task<List<Team>> Get()
+    public async Task<(List<TeamDTO>, string?)> Get()
     {
         var entities = await _dbContext.Teams.ToListAsync();
 
-        return entities;
+        return (entities, null);
     }
 
-    public async Task<Team?> Get(long id)
+    public async Task<(TeamDTO?, string?)> Get(long id)
     {
         var entity = await _dbContext.Teams.Where(x => x.Id == id).FirstOrDefaultAsync();
 
-        return entity;
+        return (entity, null);
     }
 }
